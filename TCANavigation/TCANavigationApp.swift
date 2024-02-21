bb519942c296613b350af4b65668d5e25981492c
Andrii Kvashuk <andremtcp2015a@gmail.com>
2024-02-21 17:43:12 +0000

    Initial Commit

---
+++ TCANavigation/TCANavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCANavigationApp.swift
+//  TCANavigation
+//
+//  Created by Andrii Kvashuk on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct TCANavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
